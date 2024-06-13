fc7d2ba2a178ccf0590996a8e1d47f0ebe716853
Иван Непомилуев <nepomiluev.ivan@gmail.com>
2024-06-13 22:20:59 +0200

    3

---
+++ oop_03/Company.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_03
+{
+    internal class Company
+    {
+        private Person[]? personal;
+
+        public Person[]
+        public Person this[int index];
+        {
+            get => personal![index];
+            set => personal = value;
+        }
+    }
+}
