6302d19be59b0ace47e8ce11853d9d8c24a1db6a
NikSi039 <sidorova9999@internet.ru>
2024-06-22 20:45:28 +0200

    07_09

--- oop_L07_T09_наследование/Program.cs
+++ oop_L07_T09_наследование/Program.cs
@@ -42,11 +42,32 @@
 
 // практика
 
-Student vasia = new Student("Иванов", "Василий", 4.5);
+Student vasia = new Student("Иванов", "Василий", "21ПОБ21", 4.5);
 Console.WriteLine(vasia.GetScholarship());
 
 Aspirant borya = new Aspirant("Петров", "Борис", 5, "Геофизика");
 Console.WriteLine(borya.GetScholarship());
+
+Student[] group = new Student[4];
+group[0] = vasia;
+group[1] = borya;
+group[2] = new Student("Фёдоров", "Илья", "22ПОБ27", 4.9);
+Student sam = new Aspirant("Сидоров", "Сэм", 3.0, "Биология");      // восходящее преобразование
+group[3] = sam;
+//Aspirant? maks = new Student("Планк", "Макс", 5.0) as Aspirant; // as - преобразует, если одинаковые конструкторы
+//maks!.ScientificWork = "Химия";
+
+// нисходящее
+Student maks = new Student("Планк", "Макс", 5.0);
+Aspirant maksAspirant;
+if (maks is Aspirant)
+{
+    maksAspirant = (Aspirant)maks;
+    maksAspirant.ScientificWork = "Химия";
+    group[4] = maksAspirant;
+}
+Console.WriteLine(group[4].GetScholarship());
+
 
 class Student
 {
@@ -64,9 +85,9 @@ class Student
     {
         get { return averageMark; }
         set
-        { 
+        {
             if (2 <= value && value <= 5) averageMark = value;
-            else value = 0; 
+            else value = 0;
         }
     }
 
@@ -95,7 +116,7 @@ class Student
 class Aspirant : Student
 {
     public string ScientificWork { get; set; }
-    
+
     public Aspirant(string? firstName, string? lastName, double averageMark) : base(firstName, lastName, averageMark)
     {
     }
@@ -112,3 +133,4 @@ class Aspirant : Student
         return (AverageMark == 5) ? 25000 : 15000;
     }
 }
+
