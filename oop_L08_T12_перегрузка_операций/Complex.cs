115361f17d7e86b479d639aad36e9636dbaa4312
cemik <C6PC10@C6PC10>
2024-06-27 20:47:57 +0200

    08_12

---
+++ oop_L08_T12_перегрузка_операций/Complex.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_L08_T12_перегрузка_операций
+{
+    internal class Complex
+    {
+        public double Re { get; set; }
+        
+        public double Im { get; set; }
+
+        public static bool operator == (Complex A, Complex B)
+        {
+            return (A.Re == B.Re && A.Im == B.Im);
+        }
+
+        public static bool operator != (Complex A, Complex B)
+        {
+            return (A.Re == B.Re || A.Im == B.Im);
+        }
+
+        public static Complex operator + (Complex A, Complex B)
+        {
+            return new Complex
+            {
+                Re = A.Re + B.Re,
+                Im = A.Im + B.Im
+            };
+        }
+
+        public static Complex operator - (Complex A, Complex B)
+        {
+            return new Complex
+            {
+                Re = A.Re - B.Re,
+                Im = A.Im + B.Im
+            };
+        }
+
+        public static Complex operator * (Complex A, Complex B)
+        {
+            return new Complex
+            {
+                Re = (A.Re * B.Re - A.Im * B.Im),
+                Im = (A.Re * B.Im + A.Im * B.Re)
+            };
+        }
+
+        public static Complex operator / (Complex A, Complex B)
+        {
+            return new Complex
+            {
+                Re = (A.Re * B.Re + A.Im * B.Im) / (A.Re * A.Re + B.Re * B.Re),
+                Im = (A.Im * B.Re + A.Re * B.Im) / (A.Re * A.Re + B.Re * B.Re)
+            };
+        }
+
+        public override string? ToString()
+        {
+            return (Im < 0) ? $"{Re} - {-Im}i" : $"{Re} + {Im}i";
+        }
+    }
+}
--- oop_L08_T12_перегрузка_операций/Program.cs
+++ oop_L08_T12_перегрузка_операций/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using oop_L08_T12_перегрузка_операций;
+
+Complex c1 = new Complex { Re = 4, Im = 3 };
+Complex c2 = new Complex { Re = 4, Im = 3 };
+Complex c3 = new Complex { Re = 8, Im = 12 };
+
+
+Console.WriteLine(c1 == c2);
+Console.WriteLine(c1 == c3);
+
+Complex z1 = c1 + c2;
+Console.WriteLine(z1);
+
+Complex z2 = c2 + c3;
+Console.WriteLine(z2);
+
+Complex z3 = c1 * c2;
+Console.WriteLine(z3);
+
+Complex z4 = c1 / c2;
+Console.WriteLine(z4);
+
